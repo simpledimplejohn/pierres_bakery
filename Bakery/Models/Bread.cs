cdce134970702fed9eafc5e1bb6cd75ff30dd196
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-16 14:57:44 -0700

    add private int for breadPrice

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,9 +5,18 @@ namespace Bakery.TheBread
 {
     public class Bread
     {
+        public int breadNumber { get; set; }
+
+        private int _breadPrice = 5;
         public void Test() 
         {
             Console.WriteLine("Bread Class Works!");
+
+        }
+        public int breadPrice {
+            get {
+                return _breadPrice;
+            }
         }
     }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -14,12 +14,11 @@ namespace Bakery
       newPastry.Test();
       Console.WriteLine("Welcome to Pier's Bakery!");
       Console.WriteLine("We have Bread and Pastries");
-      Console.WriteLine("Our Bread cost " + 5 + " dollars");
+      Console.WriteLine("Our Bread cost " + newBread.breadPrice + " dollars");
       Console.WriteLine("Our Pastries cost " + 2 + " dollars");
       Console.WriteLine("How many loaves of bread would you like to buy?");
-      int breadNumber = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("You want " + breadNumber + " loaves of bread");
-
+      newBread.breadNumber = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("You want " + newBread.breadNumber + " loaves of bread");
     }
   }
 }
