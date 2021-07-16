1fffe0752a0a71c3f4bd4346f3cd7718f1e1a20d
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-16 14:42:51 -0700

    update main with console.WriteLine intro

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,6 +12,14 @@ namespace Bakery
       Pastry newPastry = new Pastry();
       newBread.Test();
       newPastry.Test();
+      Console.WriteLine("Welcome to Pier's Bakery!");
+      Console.WriteLine("We have Bread and Pastries");
+      Console.WriteLine("Our Bread cost " + 5 + " dollars");
+      Console.WriteLine("Our Pastries cost " + 2 + " dollars");
+      Console.WriteLine("How many loaves of bread would you like to buy?");
+      int breadNumber = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("You want " + breadNumber + " loaves of bread");
+
     }
   }
 }
