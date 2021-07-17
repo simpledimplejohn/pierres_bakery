8dd70aa0a1b27c5c3bca01387af999c97055c82a
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-17 10:20:22 -0700

    refactor BRunBakery class

--- Bakery/Models/BakeryRun.cs
+++ Bakery/Models/BakeryRun.cs
@@ -8,10 +8,18 @@ namespace Bakery.TheBakery
   {
     Bread newBread = new Bread();
     Pastry newPastry = new Pastry();
+    private double BreadPrice = 5;
+    private double PastryPrice = 2;
+    private double PastryDiscount = 5;
+    public double BreadNumber { get; set; }
+    public double PastryNumber { get; set; }
+
     public double customerPrice { get; set; }
     public void customerCalculation() 
     {
-      customerPrice = newBread.breadCalcPrice() + newPastry.pastryCalcPrice();
+      customerPrice = 
+      newBread.breadCalcPrice(BreadNumber, BreadPrice) + 
+      newPastry.pastryCalcPrice(PastryNumber, PastryPrice, PastryDiscount);
     }
   }
 }
