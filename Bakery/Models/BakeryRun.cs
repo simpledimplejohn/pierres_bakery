951478010653d211787237a506b7b61dc740720b
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-17 10:44:30 -0700

    update BakeryRun values to private

--- Bakery/Models/BakeryRun.cs
+++ Bakery/Models/BakeryRun.cs
@@ -8,18 +8,29 @@ namespace Bakery.TheBakery
   {
     Bread newBread = new Bread();
     Pastry newPastry = new Pastry();
-    private double BreadPrice = 5;
-    private double PastryPrice = 2;
-    private double PastryDiscount = 5;
-    public double BreadNumber { get; set; }
-    public double PastryNumber { get; set; }
+    public double _breadPrice { get; }
+    private double _pastryPrice { get; }
+    private double _pastryDiscount { get; }
+    public double breadNumber { get; set; }
+    public double pastryNumber { get; set; }
 
     public double customerPrice { get; set; }
+    // public double BreadPrice {
+    //   get {
+    //     return _BreadPrice;
+    //   }
+    // }
+    public RunBakery(double breadPrice, double pastryPrice, double pastryDiscount)
+    {
+      _breadPrice = breadPrice;
+      _pastryPrice = pastryPrice;
+      _pastryDiscount = pastryDiscount;
+    }
     public void customerCalculation() 
     {
       customerPrice = 
-      newBread.breadCalcPrice(BreadNumber, BreadPrice) + 
-      newPastry.pastryCalcPrice(PastryNumber, PastryPrice, PastryDiscount);
+      newBread.breadCalcPrice(breadNumber, _breadPrice) + 
+      newPastry.pastryCalcPrice(pastryNumber, _pastryPrice, _pastryDiscount);
     }
   }
 }
