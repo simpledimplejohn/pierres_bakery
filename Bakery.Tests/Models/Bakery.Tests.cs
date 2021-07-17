9a73b7c164df87a9c0b4895206bed31d489f8bd3
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-17 11:34:49 -0700

    update btest for newBread.breadCalcPrice

--- Bakery.Tests/Models/Bakery.Tests.cs
+++ Bakery.Tests/Models/Bakery.Tests.cs
@@ -4,10 +4,10 @@ using Bakery.TheBread;
 using Bakery.ThePastry;
 using Bakery.TheBakery;
 
-namespace ToDoList.Tests
+namespace Bakery.Tests
 {
   [TestClass]  // used for test class
-  public class ItemTests
+  public class BakeryTests
   {
 
     [TestMethod]  // declared method as test method
@@ -16,18 +16,27 @@ namespace ToDoList.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
       Pastry newPastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+
     [TestMethod]
     public void BakeryConstructor_CreateInstanceOfBakery_Bakery()
     {
       RunBakery newBakery = new RunBakery(5, 2, 5);
       Assert.AreEqual(typeof(RunBakery), newBakery.GetType());
 
+    }
+
+    [TestMethod]
+    public void DoesBread_CalculatePriceForZero()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(0, newBread.breadCalcPrice(0, 5));
     }
   }
 }
